eb1a698c20c620b6dddb7249ea8cce6b04ea952a
junnsp2020 <junnsp2011@yahoo.co.jp>
2020-05-14 07:04:29 +0900

    [Add] (タスク一覧を作成日時の新しい順に表示できるようにし、scopeを活用した)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,9 @@ class SessionsController < ApplicationController
   	user = User.find_by(email: session_params[:email])
 
   	if user&.authenticate(session_params[:password])
-      session[:user_id] = user_id
+      session[:user_id] = user.id
       redirect_to root_path, notice: "ログインしました。"
-　　else
+    else
 	  render :new
 	end
   end
@@ -21,6 +21,6 @@ class SessionsController < ApplicationController
 
   private
   def session_params
-  	params.require(:session).permit(:email, :password)
+  	params.require(:session).permit(:email, :password, :user_id)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_11_113918) do
+ActiveRecord::Schema.define(version: 2020_05_13_042221) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name", null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
