83b88067743ff4d1f0b0fcd80f0da8d832474733
junnsp2020 <junnsp2011@yahoo.co.jp>
2020-05-19 00:03:43 +0900

    [Add] Rspec(作成済みテストの名称が画面上に表示されていることを確認する)

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -6,13 +6,15 @@ describe "タスク管理機能", type: :system do
       user_a = FactoryBot.create(:user, name: "ユーザーA", email: "a@example.com")
       FactoryBot.create(:task, name: "最初のタスク", user: user_a)
     end
-    context "ユーザーAがログインしているとき"
+    context "ユーザーAがログインしているとき" do
       before do
-        #　ユーザーAでログインする
+        visit login_path
+        fill_in "メールアドレス", with: "a@example.com"
+        fill_in "パスワード", with: "password"
+        click_button "ログインする"
       end
-
       it "ユーザーAが作成したタスクが表示される" do
-        # 作成済みのタスクの名称が画面上に表示されることを確認
+        expect(page).to have_content "最初のタスク"
       end
     end
   end
